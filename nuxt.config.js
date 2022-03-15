a4ccf7cc717a336f228d803435c2a63ac648463e
kfungwm <kevinfung87@gmail.com>
2022-03-15 14:33:33 +0100

    d

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
+  ssr: false, // Disable Server Side rendering,
   head: {
     title: 'solanium_overview',
     htmlAttrs: {
